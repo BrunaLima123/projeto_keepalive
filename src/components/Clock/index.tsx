42876e1edba5d1dd2e9b574e5c74bcdf7d79d55b
BrunaLima123 <bruna.lima@aluno.unc.br>
2022-07-15 11:42:33 -0300

    implementando variaveis scss

--- src/components/Clock/index.tsx
+++ src/components/Clock/index.tsx
@@ -4,12 +4,12 @@ export default function Clock() {
     const date = new Date();
     return(
         <>
-            <div className={styles.timeContainer}>
+            <div className={styles.Clock}>
     <span className={styles.number}>{date.getHours()}</span>
-    <span className={styles.division}>:</span><span className={styles.number}>{date.getMinutes()}</span>
+    <span className={styles.spots}>:</span><span className={styles.number}>{date.getMinutes()}</span>
             </div>
-            <div className={styles.timeTextContainer}>
-                <span className={styles.timeTextContainer__text}>
+            <div className={styles.clockTextContainer}>
+                <span className={styles.text}>
             {`${date.toLocaleDateString('pt-br', { weekday:"long", month:"long", day:"numeric"})}
                 de ${date.getFullYear()}`
                     }
