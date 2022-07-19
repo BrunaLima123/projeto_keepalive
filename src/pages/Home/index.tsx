fe44d988b4bd9ba3e4984437fc04f1e3f7ee5bee
BrunaLima123 <bruna.lima@aluno.unc.br>
2022-07-19 10:47:24 -0300

    alterando textos home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,14 +9,14 @@ export default function Home() {
 <section className={styles.container}>
 			<Header />
 			<section className={styles.containerText}>
-			<h2>Our mission is</h2> 
-            <p>Nossa missão é</p>
-            <h1>to transform the world</h1>
-            <p>transformar o mundo</p>
-            <h1>building digital experiences</h1>
-            <p>construindo experiências digitais</p>
-            <h1>that enable our client's growth</h1>
-            <p>que permitam o crescimento dos nossos clientes</p>
+			<h1 className={styles.pFirst}>Our mission is</h1>
+            <p className={styles.pPortugues}>Nossa missão é</p>
+            <h1 className={styles.pEnglish}>to transform the world</h1>
+            <p className={styles.pPortugues}>transformar o mundo</p>
+            <h1 className={styles.pEnglish}>building digital experiences</h1>
+            <p className={styles.pPortugues}>construindo experiências digitais</p>
+            <h1 className={styles.pEnglish}>that enable our client's growth</h1>
+            <p className={styles.pPortugues}>que permitam o crescimento dos nossos clientes</p>
 			</section>
 			<Footer />
 		</section>
